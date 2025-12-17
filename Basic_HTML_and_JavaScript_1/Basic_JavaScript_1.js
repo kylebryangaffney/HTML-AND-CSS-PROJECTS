a111d7e1e915ce1ccb02e2f780ebc70a3163d429
Kyle Gaffney <135358743+kylebryangaffney@users.noreply.github.com>
2025-12-16 14:47:32 -1000

    Update Assignment JS files

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,1 +1,9 @@
 alert("Alert Pop Up window From JavaScript");
+
+function My_First_Function()
+            {
+                const String = "Kiss me I'm Irish";
+                const result = document.getElementById("Irish");
+                result.textContent = String;
+                result.style.color = "green";
+            }
